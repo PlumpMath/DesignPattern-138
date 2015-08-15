1500d7f6c87d285ebcf8b8a247c1e4a6e35f7cbe
miumiu <tangcong29@163.com>
2015-08-15 16:43:27 +0800

    add State

---
+++ State/IState.h
@@ -0,0 +1,8 @@
+
+
+public interface IState{
+	public void insertQuarter();
+	public void ejectQuarter();
+	public void turnCrank();
+	public dispense();		
+}
---
+++ State/SoldState.java
@@ -0,0 +1,10 @@
+
+
+
+
+//yes：
+public class NoQuarterState implements IState{
+	GumballMachine gumballMachine;
+
+	public NoQuarterState
+}
